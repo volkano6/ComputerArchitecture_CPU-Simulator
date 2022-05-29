137d646e3c009a0fea1e88526e6625286a539e92
Volkan Öztoklu <volkanoztoklu@gmail.com>
2022-05-29 15:01:55 +0300

    Program is ok but have a some bug(restore file)

--- ComputerArchitecture/Simulator.py
+++ ComputerArchitecture/Simulator.py
@@ -1,20 +1,74 @@
 import numpy as np
-import Assembler
-import math
 
 
 def simulator():
-
-    RAM = []
+    RAM = [0] * 20
     ROM = []
     PC = 0
     ACC = 0
+    opcode = None
+    rest = 0
 
     ROM = np.loadtxt('instructions.txt', dtype=str)
     binary_instructions = hextoBin(ROM)
+    print("   ------------- ROM -------------")
+    for i in range(len(ROM)):
+        print("  ", i, ":", "0x", ROM[i], "-->", binary_instructions[i])
 
-    print("HEX ARRAY: " , ROM)
-    print("BINARY ARRAY: " , binary_instructions)
+    for step in range(len(ROM)):
+        print("\n")
+        print("Step", step + 1, ":")
+
+        opcode = (binary_instructions[step][0:4])
+        rest = (binary_instructions[step][4:])
+
+        if binaryToDecimal(int(rest)) == 0:
+            rest = 0
+        else:
+            rest = rest.lstrip("0")
+        rest = binaryToDecimal(int(rest))
+
+        if opcode == "0000":
+            if ACC == 0:
+                PC = PC + rest
+        elif opcode == "0001":
+            if ACC < 0:
+                PC = PC + rest
+        elif opcode == "0010":
+            ACC = rest
+        elif opcode == "0011":
+            ACC = RAM[rest]
+        elif opcode == "0100":
+            RAM[rest] = ACC
+        elif opcode == "0101":
+            ACC = ACC + RAM[rest]
+        elif opcode == "0110":
+            ACC = ACC - RAM[rest]
+        elif opcode == "0111":
+            ACC = ACC * RAM[rest]
+        elif opcode == "1000":
+            ACC = ACC / RAM[rest]
+        elif opcode == "1001":
+            ACC = 0 - ACC
+        elif opcode == "1010":
+            ACC = ACC << rest
+        elif opcode == "1011":
+            ACC = ACC >> rest
+        elif opcode == "1100":
+            ACC = ACC ^ RAM[rest]
+        elif opcode == "1101":
+            ACC = ~ACC
+        elif opcode == "1110":
+            ACC = ACC & RAM[rest]
+        elif opcode == "1111":
+            ACC = ACC | RAM[rest]
+
+        print("ACC :", ACC)
+        print("PC : ", PC, "-->   0 x", ROM[step])
+        print(RAM)
+        PC += 1
+
+
 
 def hextoBin(hexArr):
     binArr = []
@@ -29,97 +83,21 @@ def hextoBin(hexArr):
 
         binData = bin(int(my_hexdata, scale))[2:].zfill(num_of_bits)
         binArr.append(binData)
-    #print(binArr)
+    # print(binArr)
     return binArr
 
 
-
-
-def BRZ(x, ACC, RAM, PC):
-    if ACC == 0:
-        PC += x
-
-def BRN(x, ACC, RAM, PC):
-    if ACC < 0:
-        PC += x
-
-def LDI(x, ACC, RAM, PC):
-    ACC == x
-
-def LDM(x, ACC, RAM, PC):
-    ACC == RAM[x]
-
-def STR(x, ACC, RAM, PC):
-    RAM[x] == ACC
-
-def ADD(x, ACC, RAM, PC):
-    ACC == ACC + RAM[x]
-
-def SUB(x, ACC, RAM, PC):
-    ACC == ACC - RAM[x]
-
-def MUL(x, ACC, RAM, PC):
-    ACC == ACC * RAM[x]
-
-def DIV(x, ACC, RAM, PC):
-    ACC == ACC / RAM[x]
-
-def NEG(x, ACC, RAM, PC):
-    ACC == -ACC
-
-def LSL(x, ACC, RAM, PC):
-    ACC == ACC << x
-
-def LSR(x, ACC, RAM, PC):
-    ACC == ACC >> x
-
-def XOR(x, ACC, RAM, PC):
-    ACC == ACC ^ RAM[x]
-
-def NOT(x, ACC, RAM, PC):
-    ACC != ACC
-
-def AND(x, ACC, RAM, PC):
-    ACC == ACC and RAM[x]
-
-def OR(x, ACC, RAM, PC):
-    ACC == ACC or RAM[x]
-
-
-def getOpCode(opcode, x, ACC, RAM, PC):
-
-  if opcode == "0000":
-      BRZ(x, ACC, RAM, PC)
-  elif opcode == "0001":
-      BRN(x, ACC, RAM, PC)
-  elif opcode == "0010":
-      LDI(x, ACC, RAM, PC)
-  elif opcode == "0011":
-      LDM(x, ACC, RAM, PC)
-  elif opcode == "0100":
-      STR(x, ACC, RAM, PC)
-  elif opcode == "0101":
-      ADD(x, ACC, RAM, PC)
-  elif opcode == "0110":
-      SUB(x, ACC, RAM, PC)
-  elif opcode == "0111":
-      MUL(x, ACC, RAM, PC)
-  elif opcode == "1000":
-      DIV(x, ACC, RAM, PC)
-  elif opcode == "1001":
-      NEG(x, ACC, RAM, PC)
-  elif opcode == "1010":
-      LSL(x, ACC, RAM, PC)
-  elif opcode == "1011":
-      LSR(x, ACC, RAM, PC)
-  elif opcode == "1100":
-      XOR(x, ACC, RAM, PC)
-  elif opcode == "1101":
-      NOT(x, ACC, RAM, PC)
-  elif opcode == "1110":
-      AND(x, ACC, RAM, PC)
-  elif opcode == "1111":
-      OR(x, ACC, RAM, PC)
+def binaryToDecimal(binary):
+    binary1 = binary
+    decimal, i, n = 0, 0, 0
+    while (binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary // 10
+        i += 1
+        # if decimal == 0:
+        #     print("decimal : ", 0)
+    return decimal
 
 
 if __name__ == '__main__':
